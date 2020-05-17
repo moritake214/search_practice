d9353962139a5e06b49fdfaa5f30983514b38ede
Moritake Kuboichi <moritakekuboichi@mbp.local>
2020-05-17 18:09:54 +0900

    不要なものを削除

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,5 @@
 <template>
   <div class="container">
-    <!-- <select v-model="selectCategory">
-      <option value="">カテゴリー</option>
-      <option v-for="(value,key) in categories" :key="key">
-          {{ value }}
-      </option>
-    </select> -->
     <Lists></Lists>
   </div>
 </template>
@@ -20,52 +14,9 @@ export default {
     Lists
   },
   data() {
-    return {
-      eventData:{
-        target: []
-      },
-      selectCategory: '',
-      categories: {
-          1:'カテゴリー1',
-          2:'カテゴリー2',
-          3:'カテゴリー3'
-      },
-    }
   },
-  computed: {
-    filteredTasks: function () {
-      var data = this.tasks;
-      // var filterWord = this.searchWord && this.searchWord.toLowerCase();
-      var filterCategory = this.selectCategory && Number(this.selectCategory);
-      if(this.selectCategory) {
-          data = data.filter(function (row) {
-              // カテゴリー絞り込み
-              if (filterCategory) {
-                  if (row['category'] !== filterCategory) {
-                      return false;
-                  }
-              }
-              // キーワード絞り込み
-              // if (filterWord) {
-              //     return Object.keys(row).some(function (key) {
-              //         if (String(row[key]).toLowerCase().indexOf(filterWord) > -1) {
-              //             return true;
-              //         }
-              //     });
-              // }
-              // 
-              return row;
-          })
-      }
-      return data;
-    }
+  computed: { 
   }
 }
 
 </script>
-
-<style>
-  .container{
-    display: flex;
-  }
-</style>
